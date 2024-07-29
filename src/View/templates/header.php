b8cbb933066131b7731105471b48d01d71d9be18
ramaaanr <ramanoorrizki@gmail.com>
2024-07-29 19:28:51 +0800

    feat: dashboard view for admin

--- src/View/templates/header.php
+++ src/View/templates/header.php
@@ -6,5 +6,7 @@
     <title>Inventaris Lab Kesehatan</title>
     <link rel="icon" href="/public/favicon.ico" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
 </head>
 <body>
